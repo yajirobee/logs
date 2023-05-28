e0e25c8166653fc58f2fafaf7a95bafe66779f56
keisuke suzuki <keisukes0714@gmail.com>
2023-05-28 22:53:10 +0900

    update

--- _plugins/git-mtime.rb
+++ _plugins/git-mtime.rb
@@ -8,12 +8,17 @@ module Jekyll
 
             git_log_str = `git log -1 --format=format:"%ct" -- #{path}`
             raise GitError, "git log failed" if ($? != 0 || git_log_str.empty?)
-
+            
             return Time.at git_log_str.to_i
         end
     end
 
     Hooks.register(:pages, :post_init, priority: Hooks::PRIORITY_MAP[:low]) do |page|
+        # set mtime to the time of the last commit related to the current page
+        page.data['mtime'] = GitMtime.get_mtime(page.path)
+    end
+
+    Hooks.register(:posts, :post_init, priority: Hooks::PRIORITY_MAP[:low]) do |page|
         # set mtime to the time of the last commit related to the current page
         page.data['mtime'] = GitMtime.get_mtime(page.path)
     end
